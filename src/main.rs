bfdb47eaa424464caf25c271b02f71b5631e123c
delbertbeta <delbertbeta@live.com>
2022-03-27 00:39:24 +0800

    feat: add trace log

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod db;
 mod route;
 
 use std::net::SocketAddr;
+use tracing;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
@@ -10,7 +11,7 @@ async fn main() {
 
     tracing_subscriber::registry()
         .with(tracing_subscriber::EnvFilter::new(
-            std::env::var("RUST_LOG").unwrap_or_else(|_| "sso_rs=debug".into()),
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "sso_rs=debug,tower_http=trace".into()),
         ))
         .with(tracing_subscriber::fmt::layer())
         .init();
@@ -18,7 +19,7 @@ async fn main() {
     let app = route::get_app().await;
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
-    println!("listening on {}", addr);
+    tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
--- src/route/mod.rs
+++ src/route/mod.rs
@@ -1,6 +1,7 @@
 use crate::db::connection;
 use axum::extract::Extension;
 use axum::{routing::get, Router};
+use tower_http::trace::TraceLayer;
 
 mod hello_world;
 
@@ -10,4 +11,5 @@ pub async fn get_app() -> Router {
     Router::new()
         .route("/", get(hello_world::handler))
         .layer(Extension(conn))
+        .layer(TraceLayer::new_for_http())
 }
