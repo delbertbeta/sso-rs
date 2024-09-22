b9f63412186ad8e0b0faf0ad7f16fbc300fc74e2
delbertbeta <delbertbeta@live.com>
2024-09-22 14:26:32 +0800

    feat: implement get user info rpc call

--- src/rpc/user.rs
+++ src/rpc/user.rs
@@ -1,10 +1,14 @@
 use async_redis_session::RedisSessionStore;
 use async_session::SessionStore;
+use pilota::AHashMap;
 use sea_orm::DatabaseConnection;
+use volo_gen::sso::rs::User;
 
 use crate::{
+    constants::ENVS,
     error::{AppError, ServiceError},
     extractor::user_id_from_session::UserIdFromSession,
+    model::user::UserModel,
 };
 
 pub struct S {
@@ -52,11 +56,40 @@ impl volo_gen::sso::rs::UserService for S {
 
     async fn get_user(
         &self,
-        _req: ::volo_grpc::Request<volo_gen::sso::rs::GetUserRequest>,
+        req: ::volo_grpc::Request<volo_gen::sso::rs::GetUserRequest>,
     ) -> ::std::result::Result<
         ::volo_grpc::Response<volo_gen::sso::rs::GetUserResponse>,
         ::volo_grpc::Status,
     > {
-        ::std::result::Result::Ok(::volo_grpc::Response::new(Default::default()))
+        let user = UserModel::new(&self.conn);
+        let mut users = AHashMap::<i32, User>::new();
+
+        for user_id in &req.get_ref().user_id {
+            match user.find_one_user_by_id(user_id).await {
+                Ok(query_res) => {
+                    if query_res.is_some() {
+                        let (user_model, face_model) = query_res.unwrap();
+                        let face_url: Option<String> = face_model
+                            .and_then(|f| Some(format!("{}{}", ENVS.cdn_base_url, f.path)));
+
+                        users.insert(
+                            user_id.clone(),
+                            User {
+                                id: user_model.id,
+                                username: user_model.username.into(),
+                                face_url: face_url.unwrap_or("".into()).into(),
+                                self_info: user_model.self_info.unwrap_or("".into()).into(),
+                                nickname: user_model.nickname.into(),
+                            },
+                        );
+                    }
+                }
+                Err(_) => (),
+            }
+        }
+
+        Ok(::volo_grpc::Response::new(
+            volo_gen::sso::rs::GetUserResponse { users },
+        ))
     }
 }
